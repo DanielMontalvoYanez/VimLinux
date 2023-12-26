39353af6830c0a1a275ea53748b667b4df1f6d1b
user03 <daniel.contacto002@gmail.com>
2023-12-25 22:54:10 -0500

    add obj

--- exampleconfig.js
+++ exampleconfig.js
@@ -1,6 +1,7 @@
 
 const func =()=>{
-   return "[env: production]";
+	let records ={ record02:"success entry",record01:["env: production00_1","simpletext"]}
+   return records.record01[0];
 }
 
 console.log(func());
